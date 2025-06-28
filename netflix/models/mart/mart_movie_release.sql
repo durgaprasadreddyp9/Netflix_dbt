f7a1eaa3e0aabddd5fe7fd40d6df2c5baa795c41
durgaprasadreddyp9 <140272330+durgaprasadreddyp9@users.noreply.github.com>
2025-06-28 12:45:55 -0500

    Initial commit of dbt project

---
+++ netflix/models/mart/mart_movie_release.sql
@@ -0,0 +1,20 @@
+{{
+    config(
+        materialized = 'table'
+    )
+}}
+
+WITH fct_ratings AS(
+    SELECT * FROM {{ ref('fct_ratings') }}
+),
+seed_dates AS (
+    SELECT * FROM {{ ref('seed_movie_release_dates') }}
+)
+
+SELECT f.*,
+    CASE
+        WHEN d.release_date IS NULL THEN 'Unknown' ELSE 'know'
+    END AS release_info
+FROM fct_ratings f 
+LEFT JOIN seed_dates d 
+ON f.movie_id = d.movie_id 
